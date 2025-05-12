e3f6e33cd9758becb7b8b24f62b4e97ca85046f0
Antony <antonyayansi@gmail.com>
2025-05-12 15:22:58 -0500

    fix: remove middleware

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-export function onRequest(context, next) {
-  // intercept response data from a request
-  // optionally, transform the response
-  // return a Response directly, or the result of calling `next()`
-
-  console.log('Middleware: onRequest')
-
-  return next()
-}
